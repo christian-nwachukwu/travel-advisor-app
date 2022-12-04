adbcaba3505fd402f434a2fc6da62c720e72a824
christian nwachukwu <christian.nwachukwu2020@gmail.com>
2022-12-04 04:57:24 +0100

    working on the app.css and navbar.scss

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,53 @@
 import './navbar.css'
 import {SiYourtraveldottv} from 'react-icons/si'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
     return(
         <section className='navBarSection'>
             <div className='header'>
                 <div className='logoDiv'>
-                    <a href="#" className="logo">
-                        <h1><SiYourtraveldottv className="icon"/>Safari</h1>
-                    </a>
+                <a href="#" className="logo">
+                        <h1 className='flex'><SiYourtraveldottv className="icon"/>Safari</h1>
+                    </a>   
+                </div>
+
+                <div className='navBar'>
+                    <ul className='navList flex'>
+                        <li className='navItem'>
+                            <a href="#" className='navLink'>Home</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href="#" className='navLink'>Products</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href="#" className='navLink'>Resources</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href="#" className='navLink'>Contacts</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href="#" className='navLink'>Blog</a>
+                        </li>
+
+                        <div className='headerBtns flex'>
+                            <button className='btn loginBtn'>
+                                <a href="#">Login</a>
+                            </button>
+                            <button className='btn'>
+                                <a href="#">Sign Up</a>
+                            </button>
+                        </div>
+                    </ul>
+
+                    <div className='closeNavbar'>
+                        <AiFillCloseCircle className='icon'/>
+                    </div>
+                </div>
+
+                <div className='toggle'>
+                    <TbGridDots className='icon'/>
                 </div>
             </div>
         </section>
